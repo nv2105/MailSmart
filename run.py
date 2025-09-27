2f6d041b8563075106b6d3edd9332a971e3d7e0b
Naman Vora <voranaman21904@gmail.com>
2025-09-27 12:28:51 +0530

    adding some python files in file structure

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app.services.gmail_service import get_emails_from_last_24_hours
 
 emails = get_emails_from_last_24_hours()
+
+print("\n--- MAILSMART 24-HOUR EMAIL CHECK ---\n")
 for e in emails:
     print("📩 From:", e['from'])
     print("📝 Subject:", e['subject'])
     print("🔍 Snippet:", e['snippet'])
-    print("-----") 
+    print("-----")
